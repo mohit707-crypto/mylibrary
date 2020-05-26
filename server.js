b5dbbaa38e9d4c0169e3947ffde430f1fa928d81
mohit-crypto <mohit_khandar@yahoo.com>
2020-05-26 05:56:56 +0530

    Books Create/Index/New

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ if(process.env.NODE_EN !== 'production'){
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors') 
+const bookRouter = require('./routes/books') 
 
 app.set('view engine','ejs')
 app.set('views', __dirname+'/views')
 app.set('layouts','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended:false }))
 
 const mongoose = require('mongoose')
 
@@ -23,6 +27,10 @@ mongoose.connect(process.env.DATABASE_URL,{
     const db = mongoose.connection
     db.on('error', error => console.error(error))
     db.once('open', () => console.log('connected to mongoose'))
+
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+
  
 app.listen(process.env.PORT || 3000)
